318a76de9d69f7326c8194f6ae7dc935c59f1fba
Keerthi Suvvada <148604972+KEERTHI-SUVVADA@users.noreply.github.com>
2025-11-04 23:32:13 +0530

    Add files via upload
    Initial commit: added setup and Day1 queries

---
+++ sql queries/queries/day1.sql
@@ -0,0 +1,11 @@
+SELECT * FROM patients;
+
+SELECT * FROM patients LIMIT 5;
+
+SELECT patient_id,name,age FROM patients;
+
+SELECT * FROM services_weekly LIMIT 10;
+
+SELECT service FROM services_weekly;
+
+SELECT distinct service FROM services_weekly;
