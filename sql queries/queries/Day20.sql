6d75117ce5e2a996d941de3265f482c589bca047
Keerthi Suvvada <148604972+KEERTHI-SUVVADA@users.noreply.github.com>
2025-11-26 21:06:46 +0530

    Add files via upload

---
+++ sql queries/queries/Day20.sql
@@ -0,0 +1,46 @@
+--1.Calculate running total of patients admitted by week for each service.
+SELECT service,week,patients_admitted,
+SUM(patients_admitted) OVER (
+PARTITION BY service
+ORDER BY week) AS running_total
+FROM services_weekly;
+
+--2.Find the moving average of patient satisfaction over 4-week periods.
+SELECT service,week,patient_satisfaction,
+AVG(patient_satisfaction) OVER (
+PARTITION BY service
+ORDER BY week
+ROWS BETWEEN 3 PRECEDING AND CURRENT ROW)
+AS moving_avg
+FROM services_weekly;
+
+--3.Show cumulative patient refusals by week across all services.
+SELECT service,week,patients_refused,
+SUM(patients_refused) 
+OVER (ORDER BY week)
+AS cumulative_refusals
+FROM services_weekly;
+
+/*Challenge:
+Question: Create a trend analysis showing for each service and week:
+week number, patients_admitted, running total of patients admitted (cumulative),
+3-week moving average of patient satisfaction (current week and 2 prior weeks), 
+and the difference between current week admissions and the service average. Filter for weeks 10-20 only.
+*/
+SELECT service, week, patients_admitted,
+SUM(patients_admitted) OVER (
+PARTITION BY service
+ORDER BY week)
+AS cumulative_admissions,
+AVG(patient_satisfaction)
+OVER (PARTITION BY service
+ORDER BY week
+ROWS BETWEEN 2 PRECEDING AND CURRENT ROW)
+AS avg_satisfaction,
+patients_admitted,
+AVG(patients_admitted)
+OVER (PARTITION BY service)
+AS diff_avg
+FROM services_weekly
+WHERE week BETWEEN 10 AND 20
+ORDER BY service, week;
